f5b36f7255c291e2a916f5bfe60239d49a2438d5
Caio Vaccaro <caiovaccaromora@gmail.com>
2014-01-12 12:43:48 -0200

    Enlightenment

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,18 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      var noNuts = _(products).filter(function(prod) {
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+        if(prod.containsNuts === false) {
+          var iCanEat = _(prod.ingredients).all(function(ingr) {
+            return ingr !== 'mushrooms';
+          });
+          if(iCanEat) productsICanEat.push(prod);
+        }
+        
+      });
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +65,21 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _(_.range(1, 1000)).chain()
+               .reduce(function(memo, x) {
+                 if(x % 3 === 0 || x % 5 === 0) {
+                  return memo + x;
+                 } else {
+                  return memo;
+                 }
+               },0).value();    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,39 +92,47 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    _(products).chain()
+               .map(function(prod) {
+                  return prod.ingredients;
+               }).flatten()
+               .reduce(function(memo, x) {
+                  if(typeof memo !== 'undefined') ingredientCount[memo] = (ingredientCount[memo] || 0) + 1;
+                  ingredientCount[x] = (ingredientCount[x] || 0) + 1;
+               });
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
+  
+  // it("should find the largest prime factor of a composite number", function () {
 
-  });
+  // });
 
-  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+  // it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
 
-  });
+  // });
 
-  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+  // it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
 
 
-  });
+  // });
 
-  it("should find the difference between the sum of the squares and the square of the sums", function () {
+  // it("should find the difference between the sum of the squares and the square of the sums", function () {
 
-  });
+  // });
 
-  it("should find the 10001st prime", function () {
+  // it("should find the 10001st prime", function () {
 
-  });
-  */
+  // });
+  
 });
